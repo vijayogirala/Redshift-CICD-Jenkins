08e93f3c62ca377418b5fc57d1d1879590eb2103
SoundaryaSarla <89843642+SoundaryaSarla@users.noreply.github.com>
2022-11-29 11:30:52 +0530

    Update redshift.py

--- redshift.py
+++ redshift.py
@@ -1,15 +1,14 @@
 import psycopg2
-def handler(event,context):
-    conn = psycopg2.connect(
-        host="redshift-cluster-1.cpiazh88ds78.us-east-1.redshift.amazonaws.com",
-        database="dev",
-        user="awsuser",
-        port=5439,
-        password="Admin12345")
-    cur = conn.cursor()
-    print("connection succussfully established")
-    cur.execute("select * from users")
-    print(cur.fetchall())
-    print(".........")
-    print()
-    conn.close()
+conn = psycopg2.connect(
+    host="redshift-cluster-1.cpiazh88ds78.us-east-1.redshift.amazonaws.com",
+    database="dev",
+    user="awsuser",
+    port=5439,
+    password="Admin12345")
+cur = conn.cursor()
+print("connection succussfully established")
+cur.execute("select * from users")
+print(cur.fetchall())
+print(".........")
+print()
+conn.close()
