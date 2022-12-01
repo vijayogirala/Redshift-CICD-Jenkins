b292e1109e7bb68f858e85c9de5e05c960fc31b1
SoundaryaSarla <89843642+SoundaryaSarla@users.noreply.github.com>
2022-12-01 19:44:16 +0530

    Update redshift.py

--- redshift.py
+++ redshift.py
@@ -7,7 +7,7 @@ conn = psycopg2.connect(
     password="Admin12345")
 cur = conn.cursor()
 print("connection succussfully established")
-cur.execute("select state from users")
+cur.execute("select city from users")
 print(cur.fetchall())
 print(".........")
 print()
