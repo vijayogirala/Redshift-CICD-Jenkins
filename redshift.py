ee759281ca5243c0f3041501e855db12fb083ab9
SoundaryaSarla <89843642+SoundaryaSarla@users.noreply.github.com>
2022-12-05 12:21:48 +0530

    Update redshift.py

--- redshift.py
+++ redshift.py
@@ -6,10 +6,9 @@ conn = psycopg2.connect(
     port=5439,
     password="Admin12345")
 cur = conn.cursor()
-cur.execute("CREATE TABLE emp(id integer PRIMARY KEY, num integer, data varchar);")
 print("connection succussfully established")
-#cur.execute("select city from users")
-#print(cur.fetchall())
+cur.execute("select city from users")
+print(cur.fetchall())
 print(".........")
 print()
 conn.close()
